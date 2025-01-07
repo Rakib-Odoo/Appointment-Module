7f653ef4b3985d935da348b44ed42a991da7624c
Rakib-Odoo <shahidulhuda0@gmail.com>
2025-01-07 22:02:10 +0600

    create security groups

--- __manifest__.py
+++ __manifest__.py
@@ -14,6 +14,7 @@
     'data': [
         # security file
         'security/ir.model.access.csv',
+        'security/security_groups.xml',
 
         # views all file
         'views/appointment_view.xml',
